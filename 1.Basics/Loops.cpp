177ad8822e8f79faabc1bb6259dbf10fba8b2efe
akshat12-20 <akshatbhardwaj400@gmail.com>
2024-12-24 13:17:50 +0530

    while loop practice

--- 1.Basics/Loops.cpp
+++ 1.Basics/Loops.cpp
@@ -31,8 +31,37 @@ int main() {
 //While Loop
 
 //CODE1
+// int n = 5;
+//     int factorial = 1;
+
+//     while (n > 0) {
+//         factorial *= n;
+//         n--;
+//     }
+
+//     cout << "Factorial of 5 is: " << factorial << endl;
 
 
+//CODE2
+int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 6;
+
+    // Using 'break' to exit the loop early when the target is found
+    for (int num : numbers) {
+        if (num == target) {
+            cout << "Target found: " << target << endl;
+            break; // Exit the loop immediately
+        }
+        cout << "Checking: " << num << endl;
+    }
+
+    // Using 'continue' to skip specific elements
+    for (int num : numbers) {
+        if (num % 2 == 0) {
+            continue; // Skip even numbers
+        }
+        cout << "Odd number: " << num << endl;
+    }
 
 
     return 0;
