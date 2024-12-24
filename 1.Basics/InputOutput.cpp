a62405100f5fcf5fe97f9fcfd3e73e50f7204681
akshat12-20 <akshatbhardwaj400@gmail.com>
2024-12-24 10:57:04 +0530

    inputoutput

--- 1.Basics/InputOutput.cpp
+++ 1.Basics/InputOutput.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
-#include<math.h>
-#include<string>
+// #include<iostream>
+// #include<math.h>
+// #include<string>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+string name; 
+cout<<"Enter your name:";
+//cin>>name;// only take input upto the whitespace.
+getline(cin, name);// will take the entire string
+cout<<"your name is:"<<name;
 
-
-
+return 0;
 }
